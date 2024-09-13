7e5c8d761d0196d757046eb306c5be383365b649
Ahmed Moumad <133714300+Hmad44@users.noreply.github.com>
2024-09-13 10:45:07 -0500

    Implemented conditional control flow and logical operators

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -36,6 +36,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return expr.value;
     }
 
+    // Evaluate logical expressions
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+            if (isTruthful(left)) return left;
+        } else {
+            if (!isTruthful(left)) return left;
+        }
+
+        return evaluate(expr.right);
+    }
+
     // Evaluate conditional
     @Override
     public Object visitConditionalExpr(Expr.Conditional expr) {
@@ -207,6 +220,17 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
         evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        if (isTruthful(evaluate(stmt.condition))) {
+            execute(stmt.thenBranch);
+        } else if (stmt.elseBranch != null) {
+            execute(stmt.elseBranch);
+        }
+
         return null;
     }
 
--- lox/Parser.java
+++ lox/Parser.java
@@ -60,10 +60,25 @@ class Parser {
     }
 
     private Stmt statement() {
+        if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
         if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
         return expressionStatement();
+    }
+
+    private Stmt ifStatement() {
+        consume(LEFT_PAREN, "Expect '{' after 'if'");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after if condition.");
+
+        Stmt thenBranch = statement();
+        Stmt elseBranch = null;
+        if (match(ELSE)) {
+            elseBranch = statement();
+        }
+
+        return new Stmt.If(condition, thenBranch, elseBranch);
     }
 
     private Stmt printStatement() {
@@ -107,7 +122,7 @@ class Parser {
     }
 
     private Expr assignment() {
-        Expr expr = conditional();
+        Expr expr = or();
 
         if (match(EQUAL)) {
             Token equals = previous();
@@ -124,25 +139,29 @@ class Parser {
         return expr;
     }
 
-    // Future Support for implementing comma operator
-    /*
+    private Expr or() {
+        Expr expr = and();
 
-    private Expr expression() {
-        return comma();
-    }
-
-    private Expr comma() {
-        Expr expr = conditional();
-
-        while (match(COMMA)) {
+        while (match(OR)) {
             Token operator = previous();
-            Expr right = conditional();
-            expr = new Expr.Binary(expr, operator, right);
+            Expr right = and();
+            expr = new Expr.Logical(expr, operator, right);
         }
 
         return expr;
     }
-     */
+
+    private Expr and() {
+        Expr expr = conditional();
+
+        while (match(AND)) {
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+
+        return expr;
+    }
 
     private Expr conditional() {
         Expr expr = equality();
